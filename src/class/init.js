261f28a0d138287dbd7a20cc0ee5f5a67f06ba5d
lwd3737 <lwd3737@naver.com>
2020-07-31 18:30:12 +0900

    completed class

--- src/class/init.js
+++ src/class/init.js
@@ -1,3 +1,4 @@
+
 /* eslint-disable */
 const dancers = [];
 
@@ -10,9 +11,32 @@ function handleClickDancerButton () {
   // make a dancer with a random position
   //
   // TODO:
+  let dancer = new BlinkyDancerClass(
+    document.body.clientHeight * Math.random(),
+    document.body.clientWidth * Math.random(),
+    Math.random() * 1000
+  )
+
+  dancers.push(dancer);
+
+  document.body.append(dancer.$node);
 }
 
 window.addEventListener('DOMContentLoaded', () => {
   const elAddDancerButton = document.querySelector('.addDancerButton');
   elAddDancerButton.addEventListener('click', handleClickDancerButton);
+  
+  const lineupBtn = document.querySelector('#lineupButton');
+  lineupBtn.addEventListener('click', ev => {
+    let containerEl = document.createElement('div');
+    let innerEl = document.createElement('div');
+
+    containerEl.id = 'container';
+    innerEl.id = 'inner';
+
+    containerEl.append(innerEl);
+    document.body.append(containerEl);
+
+    dancers.forEach(dancer => dancer.lineup());
+  });
 });
